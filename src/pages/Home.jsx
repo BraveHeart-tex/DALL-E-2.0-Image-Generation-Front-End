1abdaa6341c8ff3afe6c550b663623d435d7c244
Bora <bora@Bora-MacBook-Air.local>
2023-01-21 21:39:06 +0300

    finished card component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,6 +16,29 @@ const Home = () => {
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState('');
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch('https://localhost:8080/api/v1/posts', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (response.ok) {
+          const result = await response.json();
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+  }, []);
+
   return (
     <section className='max-w-7xl mx-auto'>
       <div>
@@ -49,7 +72,7 @@ const Home = () => {
               {searchText ? (
                 <RenderCards data={[]} title='No results found' />
               ) : (
-                <RenderCards data={[]} title='No posts found' />
+                <RenderCards data={allPosts} title='No posts found' />
               )}
             </div>
           </>
